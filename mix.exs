ab0e976074b1f995765c73ba0a2983b703d837f6
Christopher Bunting <christophersbunting@gmail.com>
2016-10-17 22:00:34 -0700

    Adding Timex library

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Beat.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :timex]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule Beat.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:timex, "~> 3.0"}]
   end
 end
